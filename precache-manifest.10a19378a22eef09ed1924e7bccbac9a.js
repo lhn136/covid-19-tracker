5bc51e9a0ade34c5f4de4617889ec0944a666a40
lhn136 <lhn136@gmail.com>
2020-04-23 11:09:32 -0700

    Updates

---
+++ precache-manifest.10a19378a22eef09ed1924e7bccbac9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5aab1fbe766ada9ed56607be57cfc4",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "02e72d578500c5359a16",
+    "url": "/covid-19-tracker/static/css/main.ace255ee.chunk.css"
+  },
+  {
+    "revision": "d5e2b8f165deffd4762e",
+    "url": "/covid-19-tracker/static/js/2.9c78f70f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.9c78f70f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e72d578500c5359a16",
+    "url": "/covid-19-tracker/static/js/main.0b7470ea.chunk.js"
+  },
+  {
+    "revision": "d01d75a549a7ec555ecd",
+    "url": "/covid-19-tracker/static/js/runtime-main.7c67e4b6.js"
+  },
+  {
+    "revision": "6d26bf248c9578f2ce53c4e3c4a684a8",
+    "url": "/covid-19-tracker/static/media/image.6d26bf24.png"
+  }
+]);
